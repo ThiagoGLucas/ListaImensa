e3a645a23e30467310d14784315f9edbd8c61291
ThiagoGLucas <168426420+ThiagoGLucas@users.noreply.github.com>
2024-05-28 10:24:51 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner ref = new Scanner(System.in);
+		
+		System.out.println("Digite um comprimento em metros: ");
+		double a = ref.nextDouble();
+		
+		double cm = a * 100;
+		double mm = a * 1000;
+		
+		System.out.println("O comprimento indicado corresponde a: " + cm + " centímetros " + " e " + mm + " milimetros" );
+		
+		
+		
+	}
+}
