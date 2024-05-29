907ff9c0c2501d022107d93ba85f8a1fe4d87b9c
ThiagoGLucas <168426420+ThiagoGLucas@users.noreply.github.com>
2024-05-29 11:04:30 -0300

    Create QuestaoTrintaENove.java

---
+++ QuestaoTrintaENove.java
@@ -0,0 +1,19 @@
+public class QuestaoTrintaENove {
+
+	public static void main(String[] args) {
+		Scanner ref = new Scanner(System.in);
+		
+		System.out.println("Digite a primeira nota: ");
+		int a = ref.nextInt();
+		
+		System.out.println("Digite a segunda nota: ");
+		int b = ref.nextInt();
+		
+		System.out.println("Digite a terceira nota: ");
+		int c = ref.nextInt();
+		
+		System.out.println("nota 1: " + a + ", nota 2: " + b + ", nota 3: " + c);
+
+	}
+}
+
